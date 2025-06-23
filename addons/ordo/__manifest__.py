ddbd32022ecc15e15d43fbd1f13b92c6e2ca906c
Moctar <moctarjallo@gmail.com>
2025-06-23 06:46:02 +0000

    Update main domain module

--- addons/ordo/__manifest__.py
+++ addons/ordo/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    'name': 'Loopl',
+    'name': 'Ordo',
     'version': '18.0.1.0.0',
-    'summary': 'Demo for Loopl',
-    'category': 'Tools',
+    'summary': 'Demo for Ordo',
+    'category': 'Chatbots',
     'depends': ['base'],
     'data': [],
     'installable': True,
