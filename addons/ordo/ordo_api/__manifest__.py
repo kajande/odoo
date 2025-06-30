cd941e906b0837fe8b8f5595601c993a9eba102b
Moctar <moctarjallo@gmail.com>
2025-06-30 13:35:21 +0000

    Initial commit of domain code

---
+++ addons/ordo/ordo_api/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Ordo API',
+    'version': '18.0.1.0.0',
+    'summary': 'Main API system for Ordo',
+    'category': 'Chatbots',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
