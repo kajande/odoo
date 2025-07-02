cfaefd543d684f659ab8a962a1bc51a3a203ec20
Moctar <moctarjallo@gmail.com>
2025-07-02 01:16:29 +0000

    Add whatsapp_api app

--- addons/ordo/ordo_api/models/fastapi_endpoint.py
+++ addons/ordo/ordo_api/models/fastapi_endpoint.py
@@ -9,14 +9,22 @@ from odoo.addons.fastapi.dependencies import odoo_env
 class FastapiEndpoint(models.Model):
     _inherit = "fastapi.endpoint"
 
-    app: str = fields.Selection(
-        selection_add=[("demo", "Demo Endpoint")],
-        ondelete={"demo": "cascade"}
+    app = fields.Selection(
+        selection_add=[
+            ("demo", "Demo Endpoint"),
+            ("whatsapp", "Whatsapp Endpoint"),
+        ],
+        ondelete={
+            "demo": "cascade",
+            "whatsapp": "cascade",
+        },
     )
 
     def _get_fastapi_routers(self):
         if self.app == "demo":
             return [demo_api_router]
+        if self.app == "whatsapp":
+            return [whatsapp_api_router]
         return super()._get_fastapi_routers()
 
 demo_api_router = APIRouter()
@@ -27,6 +35,17 @@ class PartnerInfo(BaseModel):
 
 @demo_api_router.get("/partners", response_model=list[PartnerInfo])
 def get_partners(env: Annotated[Environment, Depends(odoo_env)]) -> list[PartnerInfo]:
+    return [
+        PartnerInfo(
+            name=partner.name,
+            email=partner.email or None
+        )
+        for partner in env["res.partner"].search([])
+    ]
+
+whatsapp_api_router = APIRouter()
+@whatsapp_api_router.get("/partners", response_model=list[PartnerInfo])
+def get_whatsapp_partners(env: Annotated[Environment, Depends(odoo_env)]) -> list[PartnerInfo]:
     return [
         PartnerInfo(
             name=partner.name,
